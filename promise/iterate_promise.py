fbffc24b7c0356cfa4d11b6143c705b08f8d8b13
piotrflorek <piotr.m.florek@gmail.com>
2016-11-10 10:09:03 +0000

    fix the 2.7 bug

--- promise/iterate_promise.py
+++ promise/iterate_promise.py
@@ -3,6 +3,7 @@
 
 def iterate_promise(promise):
     if not promise.is_fulfilled:
-        yield from promise.future
+        for foo in promise.future:
+            yield foo
     assert promise.is_fulfilled
     return promise.get()
